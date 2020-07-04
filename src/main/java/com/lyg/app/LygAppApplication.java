066d89eff7b64f245d835c72e02f96a2a8ae3955
iyeong-gyo <whdnseowkd@naver.com>
2020-07-04 14:29:17 +0900

    Initial commit

---
+++ src/main/java/com/lyg/app/LygAppApplication.java
@@ -0,0 +1,13 @@
+package com.lyg.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LygAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LygAppApplication.class, args);
+    }
+
+}
